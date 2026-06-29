1346042c202fe610118283ac7491da3208fef84f
FranAguilar98 <francisca.aguilar.galindo@gmail.com>
2026-06-29 14:23:33 -0400

    Update SecurityConfig to permit all requests

--- src/main/java/sistemadegestion/demo/config/SecurityConfig.java
+++ src/main/java/sistemadegestion/demo/config/SecurityConfig.java
@@ -4,24 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-            .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(authorize ->
-                authorize.anyRequest().authenticated()
-            )
-            .oauth2ResourceServer(oauth2 ->
-                oauth2.jwt(Customizer.withDefaults())
+            .authorizeHttpRequests(auth ->
+                auth.anyRequest().permitAll()
             );
 
         return http.build();
